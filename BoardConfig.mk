3333b507f9f98372a26ce7da916d16587299f068
sportsstar89 <sjpirri@gmail.com>
2013-04-14 11:03:47 -0400

    updates fro bcmdhd

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -36,7 +36,7 @@ ARCH_ARM_HAVE_VFP := true
 # Kernel
 TARGET_PREBUILT_KERNEL := device/htc/leo/prebuilt/kernel
 TARGET_KERNEL_SOURCE := kernel/htc/leo
-TARGET_KERNEL_CONFIG := leo_defconfig
+TARGET_KERNEL_CONFIG := htcleo_defconfig
 
 # FPU compilation flags
 TARGET_GLOBAL_CFLAGS += -mtune=cortex-a8 -mfpu=neon -mfloat-abi=softfp
@@ -56,23 +56,23 @@ COMMON_GLOBAL_CFLAGS    += -DREFRESH_RATE=60
 BOARD_USE_LEGACY_TRACKPAD := true
 
 # Blank unused Wi-Fi configs
-WIFI_BAND                   :=
-BOARD_HOSTAPD_DRIVER        :=
-BOARD_HOSTAPD_PRIVATE_LIB   :=
-WIFI_DRIVER_FW_PATH_P2P     :=
-WIFI_DRIVER_FW_PATH_PARAM   :=
-BOARD_LEGACY_NL80211_STA_EVENTS := false
+#WIFI_BAND                   :=
+#BOARD_HOSTAPD_DRIVER        :=
+#BOARD_HOSTAPD_PRIVATE_LIB   :=
+#WIFI_DRIVER_FW_PATH_P2P     :=
+#WIFI_DRIVER_FW_PATH_PARAM   :=
+#BOARD_LEGACY_NL80211_STA_EVENTS := false
 
 # Wi-Fi Configs
-BOARD_WPA_SUPPLICANT_DRIVER := WEXT
-BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_wext
-WPA_SUPPLICANT_VERSION      := VER_0_8_X
-BOARD_WLAN_DEVICE           := bcm4329
-WIFI_DRIVER_MODULE_PATH     := "/system/lib/modules/bcm4329.ko"
-WIFI_DRIVER_FW_PATH_STA     := "/vendor/firmware/fw_bcm4329.bin"
-WIFI_DRIVER_FW_PATH_AP      := "/vendor/firmware/fw_bcm4329_apsta.bin"
+#BOARD_WPA_SUPPLICANT_DRIVER := WEXT
+#BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_wext
+#WPA_SUPPLICANT_VERSION      := VER_0_8_X
+#BOARD_WLAN_DEVICE           := bcm4329
+#WIFI_DRIVER_MODULE_PATH     := "/system/lib/modules/bcm4329.ko"
+#WIFI_DRIVER_FW_PATH_STA     := "/vendor/firmware/fw_bcm4329.bin"
+#WIFI_DRIVER_FW_PATH_AP      := "/vendor/firmware/fw_bcm4329_apsta.bin"
 WIFI_DRIVER_MODULE_ARG      := "iface_name=wlan firmware_path=/vendor/firmware/fw_bcm4329.bin nvram_path=/proc/calibration"
-WIFI_DRIVER_MODULE_NAME     := "bcm4329"
+#WIFI_DRIVER_MODULE_NAME     := "bcm4329"
 
 # # cat /proc/mtd
 # dev:    size   erasesize  name
@@ -90,8 +90,8 @@ BOARD_USERDATAIMAGE_PARTITION_SIZE := 0x0c800000
 BOARD_FLASH_BLOCK_SIZE := 131072
 
 # GPS
-#BOARD_USES_GPSSHIM := true
-#BOARD_GPS_LIBRARIES := libgps librpc
+BOARD_USES_GPSSHIM := true
+BOARD_GPS_LIBRARIES := libgps librpc
 BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE := htcleo
 TARGET_QCOM_GPS_VARIANT  :=
 
